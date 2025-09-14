6da04e4da2bd7881508fd1a291e1e787b95444ca
de0ver <121717380+de0ver@users.noreply.github.com>
2025-09-14 11:16:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const tg_bot = require("node-telegram-bot-api");
 const vk_bot = require("node-vk-bot-api");
+const unirest = require("unirest");
+const cheerio = require("cheerio");
 
 require("dotenv").config();
 
@@ -20,23 +22,29 @@ if (VK == null)
 
 console.log('BOTS CREATED!\n' + new Date(START_DATE));
 
+VK.sendMessage(process.env.VK_USER_ID, "BOT STARTED: " + new Date(START_DATE));
+TG.sendMessage(process.env.TG_USER_ID, "BOT STARTED: " + new Date(START_DATE));
+
 TG.on("text", msg => { fromTGtoVK_Text(msg); });
 TG.on("edited_message", msg => { editedMessageTG(msg); } );
 TG.on("photo", msg => { photoMessageTG(msg); });
 TG.on("video", msg => { videoMessageTG(msg); });
 
-setInterval(deletedMessagesTG(), 5 * 60 * 1000);
+setInterval(deletedMessagesTG, 5 * 60 * 1000);
 
+VK.command('/скрепинг', async (ctx) => { await parseSite(ctx); });
 VK.event("message_new", (ctx) => { fromVKtoTG_Text(ctx); });
 
 function fromTGtoVK_Text(message) {
     if (message.chat.id == process.env.TG_CHAT_ID || message.chat.id == process.env.TG_USER_ID) {
         MESSAGES[message.message_id] = message.text;
-        VK.sendMessage(process.env.VK_USER_ID,
-            "CHAT NAME: " + (message.chat.title == undefined ? message.chat.username : message.chat.title) + "\n" +
-            "FROM: t.me/" + message.from.username + "\n" + 
-            "MESSAGE: " + message.text
-        );
+        if (message.date >= Math.floor(START_DATE / 1000)) {
+            VK.sendMessage(process.env.VK_USER_ID,
+                "CHAT NAME: " + (message.chat.title == undefined ? message.chat.username : message.chat.title) + "\n" +
+                "FROM: t.me/" + message.from.username + "\n" + 
+                "MESSAGE: " + message.text
+            );
+        }
     } else return;
 }
 
@@ -45,7 +53,7 @@ function fromVKtoTG_Text(ctx) {
         //https://id.vk.com/about/business/go/docs/ru/vkid/latest/vk-id/connection/tokens/access-token
         //every 1 hour, bot will spam all messages in poll, need fix   if (new_msg_id > last_msg_id) sendMessage
         if (ctx.message.id > LAST_VK_MESSAGE_ID && ctx.message.date >= Math.floor(START_DATE / 1000)) {
-            TG.sendMessage(process.env.TG_USER_ID, ctx.message.text);
+            TG.sendMessage(process.env.TG_CHAT_ID, ctx.message.text);
             //VK.execute("messages.delete", { 'message_ids': ctx.message.id, 'peer_id': process.env.VK_USER_ID });
             VK.sendMessage(process.env.VK_USER_ID, 
                 "MESSAGE POSTED: \n" + ctx.message.text + "\n" + 
@@ -89,6 +97,26 @@ function videoMessageTG(message) {
 function deletedMessagesTG() {
     
 }
+
+async function parseSite(ctx) {
+    // try
+    // {
+    //     let response;
+    //     if (ctx.message.text.match(/\/скрепинг\s+(\S+)/) != null)
+    //         response = await unirest.get(ctx.message.text.match(/\/скрепинг\s+(\S+)/)[1]);
+
+    //     if (response != undefined || response != null) {
+    //         let page = cheerio.load(response.body);
+    //         VK.sendMessage(process.env.VK_USER_ID, page('body'));
+    //     }
+    // }
+    // catch(e)
+    // {
+    //     console.log(e);
+    //     VK.sendMessage(process.env.VK_USER_ID, e);
+    // }
+}
+
 
 VK.startPolling((err) => {
   if (err) console.error(err);
